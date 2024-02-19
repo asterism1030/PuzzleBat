6204b495e3cd449befdd3b85638bafa44887d723
asterism1030 <rlaguswl1030@gmail.com>
2024-02-19 20:33:29 +0900

    버튼 클릭 이벤트 수정
    상속받을 Base 클래스인 ButtonEvent 생성
    - Button 클릭 이벤트 시 효과음 발생
    - 팝업창 닫기 기능 구현
    - 정지 버튼 비활/활성 문제 해결

--- PuzzleBat/Assets/Script/UI/PlayUI/Button/PlayUIBtnEvent.cs
+++ PuzzleBat/Assets/Script/UI/PlayUI/Button/PlayUIBtnEvent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayUIBtnEvent : MonoBehaviour
+public class PlayUIBtnEvent : ButtonEvent
 {
     [SerializeField]
     private PlayUI playUI;
@@ -10,15 +10,39 @@ public class PlayUIBtnEvent : MonoBehaviour
     [SerializeField]
     private GameObject pauseBtnCollider;
 
+    // TODO) 리팩토링
 
     public void OnPauseBtnClicked()
     {
+        base.OnClicked();
+
         playUI.ActivePausePopup(true);
         pauseBtnCollider.SetActive(false);
     }
 
     public void OnPausePopupQuitBtnClicked()
     {
+        base.OnClicked();
+
         InputManager.Instance.Exit();
+    }
+
+    public void OnPausePopupCloseBtnClicked()
+    {
+        base.OnClicked();
+
+        playUI.ActivePausePopup(false);
+        pauseBtnCollider.SetActive(true);
+    }
+
+    // TODO) 구현, 팝업창 띄울 시 뒤의 블록들 클릭 블락 구현
+    public void OnPausePopupBgmTgBtnClicked()
+    {
+
+    }
+
+    public void OnPausePopupSoundTgBtnClicked()
+    {
+
     }
 }
